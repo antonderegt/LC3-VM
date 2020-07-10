3d00c0490b775b8e7cf82389c04a656138c8b218
Anton <antonderegt@pm.me>
2020-07-10 22:00:52 +0200

    Add AND operation

--- main.c
+++ main.c
@@ -146,7 +146,27 @@ int main(int argc, const char* argv[])
                 }
                 break;
             case OP_AND:
-                {AND, 7}
+                {
+                    /* destination register (DR) */
+                    uint16_t r0 = (instr >> 9) & 0x7;
+                    /* first operand (SR1) */
+                    uint16_t r1 = (instr >> 6) & 0x7;
+                    /* whether we are in immediate mode */
+                    uint16_t imm_flag = (instr >> 5) & 0x1;
+
+                    if (imm_flag)
+                    {
+                        uint16_t imm5 = sign_extend(instr & 0x1F, 5);
+                        reg[r0] = reg[r1] & imm5;
+                    }
+                    else
+                    {
+                        uint16_t r2 = instr & 0x7;
+                        reg[r0] = reg[r1] & reg[r2];
+                    }
+
+                    update_flags(r0);
+                }
                 break;
             case OP_NOT:
                 {NOT, 7}
