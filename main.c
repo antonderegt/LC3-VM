2183fc70d17b84a78549c1a1d8012c5dff86b836
Anton <antonderegt@pm.me>
2020-07-11 20:56:55 +0200

    Add TRAP routines

--- main.c
+++ main.c
@@ -296,7 +296,77 @@ int main(int argc, const char* argv[])
                 }
                 break;
             case OP_TRAP:
-                {TRAP Codes 8}
+                {
+                    enum
+                    {
+                        TRAP_GETC = 0x20,  /* get character from keyboard, not echoed onto the terminal */
+                        TRAP_OUT = 0x21,   /* output a character */
+                        TRAP_PUTS = 0x22,  /* output a word string */
+                        TRAP_IN = 0x23,    /* get character from keyboard, echoed onto the terminal */
+                        TRAP_PUTSP = 0x24, /* output a byte string */
+                        TRAP_HALT = 0x25   /* halt the program */
+                    };
+
+                    switch (instr & 0xFF)
+                    {
+                        case TRAP_GETC:
+                            {
+                                /* read a single ASCII char */
+                                reg[R_R0] = (uint16_t)getchar();
+                            }
+                            break;
+                        case TRAP_OUT:
+                            {
+                                putc((char)reg[R_R0], stdout);
+                                fflush(stdout);
+                            }
+                            break;
+                        case TRAP_PUTS:
+                            {
+                                /* one char per word */
+                                uint16_t* c = memory + reg[R_R0];
+                                while (*c)
+                                {
+                                    putc((char)*c, stdout);
+                                    ++c;
+                                }
+                                fflush(stdout);
+                            }
+                            break;
+                        case TRAP_IN:
+                            {
+                                printf("Enter a character: ");
+                                char c = getchar();
+                                putc(c, stdout);
+                                reg[R_R0] = (uint16_t)c;
+                            }
+                            break;
+                        case TRAP_PUTSP:
+                            {
+                                /* one char per byte (two bytes per word)
+                                here we need to swap back to
+                                big endian format */
+                                uint16_t* c = memory + reg[R_R0];
+                                while (*c)
+                                {
+                                    char char1 = (*c) & 0xFF;
+                                    putc(char1, stdout);
+                                    char char2 = (*c) >> 8;
+                                    if (char2) putc(char2, stdout);
+                                    ++c;
+                                }
+                                fflush(stdout);
+                            }
+                            break;
+                        case TRAP_HALT:
+                            {
+                                puts("HALT");
+                                fflush(stdout);
+                                running = 0;
+                            }
+                            break;
+                    }
+                }
                 break;
             case OP_RES:
             case OP_RTI:
